0708617e15e8f617825a5e9d24cbe34ba10b5141
Travers Pinkerton <jtpinkerton@gmail.com>
2020-04-20 10:37:30 -0500

    commit http route code

---
+++ src/http/get-addrecipe/index.js
@@ -0,0 +1,18 @@
+const data = require ('@begin/data')
+
+exports.handler = async function http (req) {
+  console.log('Begin API called')
+
+  await data.set({ table: 'recipes', key: 1, recipe: 'Spaghetti Squash Pad Thai' })
+  // const msg = await data.get({ table: 'recipes', key: 1 })
+
+  return {
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: JSON.stringify({
+      msg: 'created new recipe'
+    })
+  }
+}
